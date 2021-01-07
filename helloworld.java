66b908e77cc57a125f36256abad8d200bbc476eb
shiboozle <77093046+shiboozle@users.noreply.github.com>
2021-01-07 01:23:09 -0600

    Hello Wolrd java
    Just a practice run

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+      Display();
+  }
+  public static void Display()
+  {
+    System.out.println("Hello World!");
+  }
+}
